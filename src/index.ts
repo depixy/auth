2c1959db6b7a925cdcfa7c73ed3f77788f622f0a
Joshua Avalon <git@avalon.sh>
2022-06-17 15:32:41 +0800

    feat: export AuthContext

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type {} from "@fastify/cookie";
 
 export * from "./password/index.js";
 export * from "./jwt/index.js";
+export * from "./auth-context.js";
 
 export type { DepixyAuthOptions } from "./plugin.js";
 export default plugin;
